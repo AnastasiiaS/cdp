e59dac2b8784564ff47536142df7e657f4995e1e
AnastasiiaS <dooo1606@gmail.com>
2013-08-28 18:07:31 +0300

    task Hello Spring resolved

---
+++ HelloSpring/src/main/java/epam/kh/cdp/semenova/HelloWorld.java
@@ -0,0 +1,14 @@
+package epam.kh.cdp.semenova;
+
+public class HelloWorld {
+	 private String message;
+
+	   public void setMessage(String message){
+	      this.message  = message;
+	   }
+
+	   public void getMessage(){
+	      System.out.println("Your Message : " + message);
+	   }
+
+}
